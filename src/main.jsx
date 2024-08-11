4a38bf4d452b11b1bc24c14e2ab1ee945cc7be96
Aswajith <aswajith.3747@gmail.com>
2024-08-11 22:44:35 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout />}>
+//       <Route path='' element={<Home />} />
+//       <Route path='about' element={<About />} />
+//       <Route path='contact' element={<Contact />} />
+//       <Route path='user/:userid' element={<User />} />
+//       <Route 
+//       loader={githubInfoLoader}
+//       path='github' 
+//       element={<Github />}
+//        />
+//     </Route>
+//   )
+// )
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    {/* <RouterProvider router={router} /> */}
+  </StrictMode>,
+)
